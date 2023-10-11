9c8358f1493ac3c96c11ecc4210a667a049edb33
6955266 <57985216+tjepster@users.noreply.github.com>
2023-10-11 13:48:08 +0200

    Simple wave system for enemy spawner

--- Assets/_Scripts/Characters/EnemySpawner.cs
+++ Assets/_Scripts/Characters/EnemySpawner.cs
@@ -4,38 +4,47 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject chicken;
-    [SerializeField] private GameObject cow;
+    [System.Serializable] public class Wave
+    {
+        public int spawnTime;
+        public int waveSize;
+        public GameObject enemy;
+    }
 
-    [SerializeField] private float chickenInterval = 1;
-    [SerializeField] private float cowInterval = 1;
+
+    [SerializeField] public List<Wave> waveList;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnEnemy(chickenInterval, chicken));
-        StartCoroutine(spawnEnemy(cowInterval, cow));
+        SetupWaves();
     }
 
-    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    void SetupWaves()
     {
-        yield return new WaitForSeconds(interval);
+        for (int i = 0; i < waveList.Count; i++)
+        {
+            StartCoroutine(spawnEnemy(waveList[i].spawnTime, waveList[i].waveSize, waveList[i].enemy));
+        }
+    }
 
-        GameObject newEnemy;
+    private IEnumerator spawnEnemy(float spawnTime, int waveSize, GameObject enemy)
+    {
+        yield return new WaitForSeconds(spawnTime);
 
-        float chance = Random.value;
-        if (chance<0.25f)
-            newEnemy = Instantiate(enemy, new Vector3(Random.Range(-38f, 36f), 19f, 0), Quaternion.identity);
-        else if (chance < 0.5f)
-            newEnemy = Instantiate(enemy, new Vector3(Random.Range(-38f, 36f), -21f, 0), Quaternion.identity);
-        else if (chance < 0.75f)
-            newEnemy = Instantiate(enemy, new Vector3(-38f, Random.Range(-21f, 19f), 0), Quaternion.identity);
-        else
-            newEnemy = Instantiate(enemy, new Vector3(36f, Random.Range(-21f, 19f), 0), Quaternion.identity);
+        for (int i = 0; i < waveSize; i++)
+        {
+            GameObject newEnemy;
 
-
-
-        //GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-38f, 36f) * x, Random.Range(-21f, 19f) * y, 0), Quaternion.identity);
-        StartCoroutine(spawnEnemy(interval, enemy));
+            float chance = Random.value;
+            if (chance < 0.25f)
+                newEnemy = Instantiate(enemy, new Vector3(Random.Range(-38f, 36f), 19f, 0), Quaternion.identity);
+            else if (chance < 0.5f)
+                newEnemy = Instantiate(enemy, new Vector3(Random.Range(-38f, 36f), -21f, 0), Quaternion.identity);
+            else if (chance < 0.75f)
+                newEnemy = Instantiate(enemy, new Vector3(-38f, Random.Range(-21f, 19f), 0), Quaternion.identity);
+            else
+                newEnemy = Instantiate(enemy, new Vector3(36f, Random.Range(-21f, 19f), 0), Quaternion.identity);
+        }
     }
 }
