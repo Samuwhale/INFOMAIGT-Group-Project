826a0c1eb78c7e435ef0e1dc2e1d2cd71e046497
Samuwhale <sfspithorst@gmail.com>
2023-10-17 15:07:00 +0200

    Timer polish

--- Assets/_Scripts/timerScript.cs
+++ Assets/_Scripts/timerScript.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 
 public class timerScript : MonoBehaviour
 {
-    float Timer = 0f;
+    float timer = 0f;
     public TextMeshProUGUI timerText;
     // Update is called once per frame
     void Update()
     {
-        Timer += Time.deltaTime;
-        timerText.text = Timer.ToString();
+        timer += Time.deltaTime;
+
+        // Format timer into MM:SS:SS
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
